bd0a14550cfb6ea4e3ab34b07f34acc54b2b31f7
Max-APL <maxpasten15908@gmail.com>
2024-10-23 22:15:57 -0400

    Agregacion de middleware cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 # app/main.py
 from fastapi import FastAPI
 from app.api import auth, users, projects, tools, materials, flow_tools, flow_materials
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router, prefix="/users")
@@ -11,6 +28,8 @@ app.include_router(tools.router, prefix="/tools")
 app.include_router(materials.router, prefix="/materials")
 app.include_router(flow_tools.router, prefix="/flow-tools")
 app.include_router(flow_materials.router, prefix="/flow-materials")
+
+
 
 
 if __name__ == "__main__":
